72fe05747c1b68c500aafb883559aa3675bf4342
Nora Ibrahimi <nibrahimi12@gmail.com>
2019-10-14 14:18:59 +0200

    Application organization, post requests into predication

--- app/runnable/Classification.scala
+++ app/runnable/Classification.scala
@@ -4,17 +4,16 @@ import helper.Globals
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.ml.classification.RandomForestClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
 import org.apache.spark.sql.SparkSession
 
 object Classification {
-	def model(): PipelineModel = {
+
+	def setUpModel = {
 		Logger.getLogger("org").setLevel(Level.OFF)
 		Logger.getLogger("akka").setLevel(Level.OFF)
 
 		val spark = SparkSession.builder.appName("T").master("local[*]").getOrCreate
-		import spark.implicits._
 
 		var dataframe = spark.read
 			.option("header", true)
@@ -59,15 +58,7 @@ object Classification {
 		println(accuracy * 100 + "% accuracy")
 		 */
 
-		/*
-		val newTest = Seq((2585, 139, 0, 3658, 34, 89220, 65, 44, 170629, 2732, 901, 10700, 2549))
-			.toDF("gold_per_min", "level", "leaver_status", "xp_per_min", "radiant_score", "gold_spent",
-				"deaths", "denies", "hero_damage", "tower_damage", "last_hits", "hero_healing", "duration")
-
-		val predictions = model.transform(newTest)
-		predictions.show(50)
-		*/
-
 		model
 	}
+
 }
