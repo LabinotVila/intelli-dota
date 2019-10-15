fbdaf6fc3824e579e1e81d4a04146ca7673d552b
Nora Ibrahimi <nibrahimi12@gmail.com>
2019-10-15 10:53:47 +0200

    Returning correct json for visualization

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -3,20 +3,10 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import runnable.ClassificationPredicter
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 
 @Singleton
 class MainController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-	/**
-	 * Create an Action to render an HTML page with a welcome message.
-	 * The configuration in the `routes` file means that this method
-	 * will be called when the application receives a `GET` request with
-	 * a path of `/`.
-	 */
 	def index = Action {
 		Ok("Welcome to IntelliDota")
 	}
@@ -38,11 +28,9 @@ class MainController @Inject()(cc: ControllerComponents) extends AbstractControl
 
 		val prediction = ClassificationPredicter.predict(tupleAsSeq)
 
-		import com.google.gson.{Gson, JsonParser}
-
 		val returned = prediction.get(0)
 
-		Ok(returned.toString)
-	}
+		Ok(returned)
 
+	}
 }
--- app/runnable/ClassificationPredicter.scala
+++ app/runnable/ClassificationPredicter.scala
@@ -10,12 +10,17 @@ object ClassificationPredicter {
 		val spark = SparkSession.builder.appName("T").master("local[*]").getOrCreate
 		import spark.implicits._
 
-		val model = PipelineModel.load(Globals.datasetsRoute + Globals.classificationModel)
+		val model = PipelineModel.load(Globals.MAIN_ROUTE + Globals.CLASSIFIED_MODEL)
 
 		val df = arr.toDF("gold_per_min", "level", "leaver_status", "xp_per_min", "radiant_score", "gold_spent",
 			"deaths", "denies", "hero_damage", "tower_damage", "last_hits", "hero_healing", "duration")
 
-		val predictions = model.transform(df)
+		var predictions = model.transform(df)
+
+		predictions = predictions.select("gold_per_min", "level", "leaver_status", "xp_per_min",
+			"radiant_score", "gold_spent",
+			"deaths", "denies", "hero_damage", "tower_damage", "last_hits", "hero_healing", "duration",
+			"probability", "prediction")
 
 		val json2res = predictions.toJSON.collectAsList
 
