8e15309b5b697cc2826643c930dcc4acaf4f5346
Labinot Vila <vila_labinot@hotmail.com>
2019-10-23 13:14:51 +0200

    Reorganization

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -15,46 +15,47 @@ class MainController @Inject()(cc: ControllerComponents) extends AbstractControl
 	// DOUBLE FUNCTIONALITY
 	def getColumns(kind: String) = Action {
 		kind match {
-			case "steam" => Ok(Json.toJson(Dataset.getColumns(steam)))
-			case "kaggle" => Ok(Json.toJson(Dataset.getColumns(kaggle)))
+			case "steam" => Ok(Dataset.getColumns(steam))
+			case "kaggle" => Ok(Dataset.getColumns(kaggle))
 		}
 	}
-//	}
-//	def getSample(kind: DataFrame, percentage: Double) = Action {
-//		val result = Dataset.getSample(kind, percentage / 100)
-//
-//		Ok(result)
-//	}
-//
-//	// CLASSIFICATION ONLY
-//	def getCorrelationMatrix = Action {
-//		val result = Dataset.getCorrelationMatrix(steam_dataframe)
-//
-//		Ok(result)
-//	}
-//	def postPredict(attributes: Int*) = Action {
-//		val result = Dataset.predict(spark, steam_dataframe, attributes)
-//
-//		Ok(result)
-//	}
-//	def getGroupAndCount(attribute: String, partitions: Option[Int]) = Action {
-//		attribute match {
-//			case "leaver_status"    => Ok(Statistics.getBinary(steam_dataframe, attribute))
-//			case _                  => Ok(Json.toJson(Statistics.get(spark, steam_dataframe, attribute, partitions.get)))
-//		}
-//	}
-//
+	def getSample(kind: String, percentage: Double) = Action {
+		kind match {
+			case "steam" => Ok(Dataset.getSample(steam, percentage / 100))
+			case "kaggle" => Ok(Dataset.getSample(kaggle, percentage / 100))
+		}
+	}
+	def getStages(kind: String) = Action {
+		kind match {
+			case "steam" => Ok(Dataset.getStages(Constants.CLASSIFIED_MODEL))
+			case "kaggle" => Ok(Dataset.getStages(Constants.CLUSTERED_MODEL))
+		}
+	}
+	def getCorrelationMatrix(kind: String) = Action {
+		kind match {
+			case "steam" => Ok(Dataset.getCorrelationMatrix(steam))
+			case "kaggle" => Ok(Dataset.getCorrelationMatrix(kaggle))
+		}
+	}
 
-//
-//	def getStages(path: String) = Action {
-//		val model = Dataset.getStages(path)
-//
-//		Ok(Json.toJson(model))
-//	}
-//
-//	def getCenters() = Action {
-//		val result = Dataset.getCenters
-//
-//		Ok("Y")
-//	}
+	// CLASSIFICATION ONLY
+	def postPredict(attributes: Int*) = Action {
+		val result = Dataset.predict(spark, steam, attributes)
+
+		Ok(result)
+	}
+	def getGroupAndCount(attribute: String, partitions: Option[Int]) = Action {
+		attribute match {
+			case "leaver_status"    => Ok(Statistics.getBinary(steam, attribute))
+			case _                  => Ok(Json.toJson(Statistics.get(spark, steam, attribute, partitions.get)))
+		}
+	}
+
+	// CLUSTER ONLY
+	def postCluster(attributes: Double*) = Action {
+		val result = Dataset.cluster(spark, kaggle, attributes)
+
+		Ok(result)
+	}
+
 }
--- app/utilities/Dataset.scala
+++ app/utilities/Dataset.scala
@@ -1,39 +1,16 @@
 package utilities
 
 import org.apache.spark.ml.{PipelineModel, Transformer}
+import org.apache.spark.sql.functions.typedLit
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.param.Param
+import play.api.libs.json.Json
 import org.apache.spark.ml.stat.Correlation
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types.{DoubleType, StructType}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
-import scala.util.Try
-
 object Dataset {
-	def getCorrelationMatrix(dataframe: DataFrame) = {
-		val columnNames = dataframe.schema.names.filter(col => !col.equals("radiant_win"))
-
-		val assembler = new VectorAssembler().setInputCols(columnNames).setOutputCol("features")
-		val df = assembler.transform(dataframe)
-
-		Correlation.corr(df, "features").toJSON.collectAsList().toString
-	}
-
-	def predict(spark: SparkSession, dataframe: DataFrame, s: Seq[Int]) = {
-		val RDD = spark.sparkContext.makeRDD(List(Row.fromSeq(s)))
-
-		val columns = dataframe.schema.fields.filter(x => !x.name.equals("radiant_win"))
-		val df = spark.createDataFrame(RDD, StructType(columns))
-
-		val newDF = dataframe.schema
-			.add("probability", DoubleType)
-			.add("prediction", DoubleType)
-    		.names.filter(col => !col.equals("radiant_win"))
-
-		getPredictedModel.transform(df).select(newDF.map(col): _*).toJSON.collectAsList().toString
-	}
-
+	// BOTH DATASETS
 	def getStages(path: String) = {
 		val model = PipelineModel.load(Constants.MAIN_ROUTE + path)
 
@@ -56,24 +33,53 @@ object Dataset {
 			flicker = flicker + (x.uid -> listOfMaps)
 		})
 
-		flicker
+		Json.toJson(flicker)
 	}
-
 	def getColumns(dataframe: DataFrame) = {
-		dataframe.schema.names.filter(x => !x.equals("radiant_win"))
+		Json.toJson(dataframe.schema.names.filter(x => !x.equals("radiant_win")))
 	}
-
 	def getSample(dataframe: DataFrame, percentage: Double) = {
 		dataframe.sample(percentage).toJSON.collectAsList().toString
 	}
+	def getCorrelationMatrix(dataframe: DataFrame) = {
+		val columnNames = dataframe
+			.schema.names
+			.filter(col => !col.equals("radiant_win") && !col.equals("localized_name"))
 
-	def getPredictedModel = {
-		PipelineModel.load(Constants.MAIN_ROUTE + Constants.CLASSIFIED_MODEL)
+
+		val assembler = new VectorAssembler().setInputCols(columnNames).setOutputCol("features")
+		val df = assembler.transform(dataframe)
+
+		Correlation.corr(df, "features").toJSON.collectAsList().toString
+	}
+	def getPredictedModel(path: String) = {
+		PipelineModel.load(Constants.MAIN_ROUTE + path)
 	}
 
-	def getCenters = {
-		val model = PipelineModel.load(Constants.MAIN_ROUTE + Constants.CLUSTERED_MODEL)
+	// CLASSIFICATION
+	def predict(spark: SparkSession, dataframe: DataFrame, s: Seq[Int]) = {
+		val RDD = spark.sparkContext.makeRDD(List(Row.fromSeq(s)))
 
+		val columns = dataframe.schema.fields.filter(x => !x.name.equals("radiant_win"))
+		val df = spark.createDataFrame(RDD, StructType(columns))
 
+		val newDF = dataframe.schema
+			.add("probability", DoubleType)
+			.add("prediction", DoubleType)
+			.names.filter(col => !col.equals("radiant_win"))
+
+		getPredictedModel(Constants.CLASSIFIED_MODEL).transform(df).select(newDF.map(col): _*).toJSON.collectAsList().toString
+	}
+
+	// CLUSTERING DATASET
+	def cluster(spark: SparkSession, dataframe: DataFrame, s: Seq[Double]): String = {
+		val RDD = spark.sparkContext.makeRDD(List(Row.fromSeq(s)))
+
+		val columns = dataframe.schema
+		val columnsWName = columns.add("prediction", IntegerType)
+
+		val df = spark.createDataFrame(RDD, StructType(columns.fields))
+
+		getPredictedModel(Constants.CLUSTERED_MODEL).transform(df).select(columnsWName.names.map(col): _*).toJSON.collectAsList().toString
 	}
 }
